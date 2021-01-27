5a6e0fe82c45d7b10611d5582a713d1f5d7af8fe
ZARIFMAC <uzbzarif@gmail.com>
2021-01-27 22:52:11 +0900

    all

--- exchange/main.py
+++ exchange/main.py
@@ -1,12 +1,18 @@
-import requests
-from pprint import pprint
-url = 'https://api.exchangeratesapi.io/latest'
+# import requests
+# from pprint import pprint
+# url = 'https://api.exchangeratesapi.io/latest'
 
-payload = {
-    'base':'USD',
-    'symbols':['RUB','KRW']
-}
+# payload = {
+#     'base':'USD',
+#     'symbols':['RUB','KRW']
+# }
 
-r = requests.get(url,payload)
-print(r.url)
-pprint(r.json())
+# r = requests.get(url=url,params=payload)
+# print(r.url)
+# pprint(r.json())
+
+
+def sum1(a,b=0):
+    return a+b
+
+print(sum1(4,b=5))
