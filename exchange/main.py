d1a17a2a237fdf88e0bee3c02b7a516fe1ea958e
ZARIFMAC <uzbzarif@gmail.com>
2021-01-27 22:30:04 +0900

    multply params

--- exchange/main.py
+++ exchange/main.py
@@ -1,9 +1,12 @@
 import requests
 from pprint import pprint
-url = 'https://api.exchangeratesapi.io/latest?base=USD'
+url = 'https://api.exchangeratesapi.io/latest'
 
+payload = {
+    'base':'USD',
+    'symbols':['RUB','KRW']
+}
 
-
-r = requests.get(url)
+r = requests.get(url,payload)
 print(r.url)
 pprint(r.json())
