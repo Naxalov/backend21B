3297d71d4b1bf3b2b8b6de245a61a9c7f309c288
ZARIFMAC <uzbzarif@gmail.com>
2021-01-27 22:06:44 +0900

    random

---
+++ randomuserme/main.py
@@ -0,0 +1,9 @@
+
+import requests
+
+url = 'https://randomuser.me/api/?results=5&gender=male&nat=us,gb'
+
+r = requests.get(url)
+data = r.json()['results']
+for i in data:
+    print(i['name'],i['nat'])
