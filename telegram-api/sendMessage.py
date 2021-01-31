057eed995107afb602c024de1acc3478935443f3
ZARIFMAC <uzbzarif@gmail.com>
2021-01-31 21:36:09 +0900

    sendmessage

---
+++ telegram-api/sendMessage.py
@@ -0,0 +1,9 @@
+import requests
+import json
+from pprint import pprint
+token = '1602686596:AAFiy3eteVMwvKw8RT-PU8xgZLlzHqemCK0'
+url = f'https://api.telegram.org/bot{token}/sendMessage'
+r = requests.get(url)
+# 86775091
+
+    
