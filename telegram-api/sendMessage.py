9e41f3705d10338911c0aeab9218823b7a128932
ZARIFMAC <uzbzarif@gmail.com>
2021-01-31 21:49:33 +0900

    send messages with loop

--- telegram-api/sendMessage.py
+++ telegram-api/sendMessage.py
@@ -1,9 +1,21 @@
 import requests
 import json
 from pprint import pprint
+
+def sendMsg(idx):
+    url = f'https://api.telegram.org/bot{token}/sendMessage'
+    payload = {
+    'chat_id':idx,
+    'text':'Hi!!'
+    }
+    r = requests.get(url,params=payload)
+
 token = '1602686596:AAFiy3eteVMwvKw8RT-PU8xgZLlzHqemCK0'
-url = f'https://api.telegram.org/bot{token}/sendMessage'
-r = requests.get(url)
+
+ids= [86775091]
+
 # 86775091
+for idx in ids:
+    sendMsg(idx)
 
     
