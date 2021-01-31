866a1ca1c461e34a6c9fc0b9333c2bc6b8597c0b
ZARIFMAC <uzbzarif@gmail.com>
2021-01-31 20:46:45 +0900

    result

--- telegram-api/getUpdates.py
+++ telegram-api/getUpdates.py
@@ -4,5 +4,7 @@ from pprint import pprint
 token = '1602686596:AAFiy3eteVMwvKw8RT-PU8xgZLlzHqemCK0'
 url = f'https://api.telegram.org/bot{token}/getUpdates'
 r = requests.get(url)
+print(r.url)
 data = r.json()
-pprint(data)
+updates = data['result']
+print(len(updates))
