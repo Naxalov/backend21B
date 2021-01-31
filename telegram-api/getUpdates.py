72adef64981917f894b67c3eef1b61a2ca809cfa
ZARIFMAC <uzbzarif@gmail.com>
2021-01-31 21:10:52 +0900

    get user object

--- telegram-api/getUpdates.py
+++ telegram-api/getUpdates.py
@@ -4,7 +4,10 @@ from pprint import pprint
 token = '1602686596:AAFiy3eteVMwvKw8RT-PU8xgZLlzHqemCK0'
 url = f'https://api.telegram.org/bot{token}/getUpdates'
 r = requests.get(url)
-print(r.url)
 data = r.json()
 updates = data['result']
-print(len(updates))
+
+update = updates[0]
+message = update['message']
+user = message['from']
+print(user)
