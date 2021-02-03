0d8410aba390d19d0855081ad4a699b2da9da305
ZARIFMAC <uzbzarif@gmail.com>
2021-02-03 21:27:04 +0900

    TOKEN

--- telegram-api/getUpdates.py
+++ telegram-api/getUpdates.py
@@ -1,16 +1,21 @@
 import requests
 import json
 from pprint import pprint
-token = '1602686596:AAFiy3eteVMwvKw8RT-PU8xgZLlzHqemCK0'
+import os
+
+token =os.environ['TOKEN'] 
 url = f'https://api.telegram.org/bot{token}/getUpdates'
 r = requests.get(url)
 data = r.json()
 updates = data['result']
-
+print(r.url)
+# 86775091
+# ids = 
 for update in updates:
     message = update['message']
     user = message['from']
+    user_id = user['id']
     first = user.get('first_name','')
     last = user.get('last_name','')
-    print(f'{first} {last}')
-    
+    print(user_id,end=',')
+pprint(updates)    
