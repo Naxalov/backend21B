30f4c9696d3cb699c76cf770bc95553e5def7abc
ZARIFMAC <uzbzarif@gmail.com>
2021-02-03 22:52:25 +0900

    HTML

--- telegram-api/send_markdown.py
+++ telegram-api/send_markdown.py
@@ -10,7 +10,7 @@ def sendMsg(idx,text):
     payload = {
     'chat_id':idx,
     'text':text,
-    'parse_mode':'MarkdownV2'
+    'parse_mode':'HTML'
     }
     r = requests.get(url,params=payload)
 
@@ -18,8 +18,21 @@ def sendMsg(idx,text):
 # 86775091
 
 link = '[Channel](https://t.me/codeschooluz)'
+html = """
+
+<b>bold</b>, <strong>bold</strong>
+<i>italic</i>, <em>italic</em>
+<u>underline</u>, <ins>underline</ins>
+<s>strikethrough</s>, <strike>strikethrough</strike>, <del>strikethrough</del>
+<b>bold <i>italic bold <s>italic bold strikethrough</s> <u>underline italic bold</u></i> bold</b>
+<a href="http://www.example.com/">inline URL</a>
+<a href="tg://user?id=123456789">inline mention of a user</a>
+<code>inline fixed-width code</code>
+<pre>pre-formatted fixed-width code block</pre>
+<pre><code class="language-python">pre-formatted fixed-width code block written in the Python programming language</code></pre>
+
+"""
 
 
-
-sendMsg(86775091,'~_*Hello*_ **World**~ '+link)
+sendMsg(86775091,html)
 
